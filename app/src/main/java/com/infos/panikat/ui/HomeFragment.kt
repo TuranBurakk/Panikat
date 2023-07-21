9b9e2991c2aad4b4a0e1bf3682a5f47c07bf1451
burakTuran <buraktt00@gmail.com>
2023-07-21 12:11:56 +0300

    [PanikatApp][Created base and architectural]

---
+++ app/src/main/java/com/infos/panikat/ui/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.infos.panikat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.infos.panikat.R
+import com.infos.panikat.base.BaseFragment
+import com.infos.panikat.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
